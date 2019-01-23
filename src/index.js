0f783922b09014ce7c1f5187ee211502a8915d30
zacharyAD <zacharyadcoding@gmail.com>
2019-01-23 16:14:40 -0500

    brought in api key, not working to show map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello from javascript")
+//console.log("hello from javascript")
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiemFjaGFyeWFkY29kaW5nIiwiYSI6ImNqcjlvOTIxazBjMHM0OXJwcmptMTJvemEifQ.Z4bOQIwlBC2mWcCcYyYfEA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
